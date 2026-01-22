0224c0a9ebde2e439d40dd359ebd340dda5f0cb2
ja <opilane@HP-CZC2349HTR>
2026-01-22 10:40:30 +0200

    сделал миграцию. Теперь она к счастью работает

---
+++ KeelteKooli/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace KeelteKooli.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<KeelteKooli.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(KeelteKooli.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
