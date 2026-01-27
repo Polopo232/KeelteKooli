4ed90df242d4a5b44b547fb19288bba791d2827f
JA <opilane@HP-CZC2349HT2>
2026-01-27 14:53:18 +0200

    обновление моделей

--- KeelteKooli/Models/IdentityModels.cs
+++ KeelteKooli/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace KeelteKooli.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Course> Courses { get; set; }
-        public DbSet<Training> Trainings { get; set; }
+        public DbSet<Training> Training { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Registration> Registrations { get; set; }
 
--- KeelteKooli/Models/Training.cs
+++ KeelteKooli/Models/Training.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,12 @@ namespace KeelteKooli.Models
     public class Training
     {
         public int Id { get; set; }
-        public int CourseId { get; set; }
+
+        [Display(Name = "Kursuse nimetus")]
+        public string CourseName { get; set; }
+
         public int TeacherId { get; set; }
+
 
         public DateTime AlgusKuupaev { get; set; }
         public DateTime LoppKuupaev { get; set; }
