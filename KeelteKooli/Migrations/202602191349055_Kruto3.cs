1e9d640c5d52d65402e8e955e533ec32b8e4cf1a
opilane <opilane@HP-CZC2349HT2>
2026-02-19 15:51:16 +0200

    базы

---
+++ KeelteKooli/Migrations/202602191349055_Kruto3.cs
@@ -0,0 +1,21 @@
+﻿namespace KeelteKooli.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class Kruto3 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Trainings", "CourseKeel", c => c.String());
+            AddColumn("dbo.Trainings", "CourseTase", c => c.String());
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.Trainings", "CourseTase");
+            DropColumn("dbo.Trainings", "CourseKeel");
+        }
+    }
+}
+
