d40d43bbb33a40de0c9b289985b6b24e9e34b3f8
opilane <opilane@HP-CZC2349HT2>
2026-02-19 15:31:01 +0200

    новые базы

---
+++ KeelteKooli/Migrations/202602191229007_Kruto.cs
@@ -0,0 +1,26 @@
+﻿namespace KeelteKooli.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Kruto : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Registrations", "ApplicationUserId", "dbo.AspNetUsers");
+            DropIndex("dbo.Registrations", new[] { "ApplicationUserId" });
+            AlterColumn("dbo.Registrations", "ApplicationUserId", c => c.String(nullable: false, maxLength: 128));
+            CreateIndex("dbo.Registrations", "ApplicationUserId");
+            AddForeignKey("dbo.Registrations", "ApplicationUserId", "dbo.AspNetUsers", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Registrations", "ApplicationUserId", "dbo.AspNetUsers");
+            DropIndex("dbo.Registrations", new[] { "ApplicationUserId" });
+            AlterColumn("dbo.Registrations", "ApplicationUserId", c => c.String(maxLength: 128));
+            CreateIndex("dbo.Registrations", "ApplicationUserId");
+            AddForeignKey("dbo.Registrations", "ApplicationUserId", "dbo.AspNetUsers", "Id");
+        }
+    }
+}
---
+++ KeelteKooli/Migrations/202602191249541_Kruto2.cs
@@ -0,0 +1,18 @@
+﻿namespace KeelteKooli.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Kruto2 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Registrations", "Staatus", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Registrations", "Staatus", c => c.String());
+        }
+    }
+}
