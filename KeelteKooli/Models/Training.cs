9854400d0008ef8944b8f8444620e901c2af4252
opilane <opilane@HP-CZC2349HT2>
2026-03-04 15:46:37 +0200

    немного измененные модели

--- KeelteKooli/Models/Training.cs
+++ KeelteKooli/Models/Training.cs
@@ -27,6 +27,8 @@ namespace KeelteKooli.Models
         public int MaxOsalejaid { get; set; }
 
         public virtual Course Course { get; set; }
+
+        public int CourseId { get; set; }
         public virtual Teacher Teacher { get; set; }
         public virtual ICollection<Registration> Registrations { get; set; } = new List<Registration>();
     }
